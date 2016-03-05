516edecfbf6e1d6baa49455c5d226c35c4c7d173
Diogo Cezar Teixeira Batista <diogo@diogocezar.com>
2016-03-05 18:45:50 -0300

    Add sendgrid support

--- config/config.php
+++ config/config.php
@@ -10,25 +10,21 @@
 	/**
 	* Report title
 	*/
-
 	$title_report = "Daily Report of Sites Status";
 
 	/**
 	* Label sites online
 	*/
-
 	$label_online = "Sites that have been online";
 
 	/**
 	* Label sites offline
 	*/
-
 	$label_offline = "Sites that have been offline";
 
 	/**
 	* Hour to send report
 	*/
-
 	$hour_send_report = 16;
 
 	/**
@@ -44,33 +40,38 @@
 	/**
 	* Emails to send report
 	*/
-
 	$emails_report = array(
 		'email1@gmail.com',
 		'email2@gmail.com'
 	);
 
 	/**
+	* Mail Type Key to send an Email
+	*/
+	$mail_type = "sendgrid";
+
+	/**
 	* Mandrill Key to send an Email
 	*/
-
 	$mandrill_key = "";
+
+	/**
+	* SendGrid Key to send an Email
+	*/
+	$sendgrid_key = "";
 
 	/**
 	* Sent successfully
 	*/
-
 	$sent_successfully = "Email has been sent successfully.";
 
 	/**
 	* Error to send
 	*/
-
 	$send_error = "Sorry, the email can not be sent.";
 
 	/**
 	* Debug
 	*/
-
 	$debug = false;
 ?>
