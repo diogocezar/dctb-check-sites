19b498d488f9f5a3a7c8819343b98c2cb214fadf
Diogo Cezar Teixeira Batista <diogo@diogocezar.com>
2016-03-06 02:27:44 -0300

    config

---
+++ config/config.php
@@ -0,0 +1,86 @@
+<?php
+	/**
+	* Sites to check
+	*/
+	$sites = array(
+		'http://www.diogocezar.com',
+		'http://www.padariajoia.com.br',
+		'http://www.creativetroops.com.br'
+	);
+
+	/**
+	* Report title
+	*/
+
+	$title_report = "DCBuster Server - Daily Report of Sites Status";
+
+	/**
+	* Label sites online
+	*/
+
+	$label_online = "Sites that have been online";
+
+	/**
+	* Label sites offline
+	*/
+
+	$label_offline = "Sites that have been offline";
+
+	/**
+	* Hour to send report
+	*/
+
+	$hour_send_report = 2;
+
+	/**
+	* From email
+	*/
+	$from_email = "xgordo@gmail.com";
+
+	/**
+	* From name
+	*/
+	$from_name = "DCBuster Server";
+
+	/**
+	* Emails to send report
+	*/
+
+	$emails_report = array('xgordo@gmail.com' => 'Diogo Cezar');
+
+	/**
+	* Email type
+	*/
+
+	$mail_type = "sendgrid";
+
+	/**
+	* SendGrid Key to send an Email
+	*/
+
+	$sendgrid_key = "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";
+
+	/**
+	* Mandrill Key to send an Email
+	*/
+
+	$mandrill_key = "RbFB5br4ZITKzG8E65pEPQ";
+
+	/**
+	* Sent successfully
+	*/
+
+	$sent_successfully = "Email has been sent successfully.";
+
+	/**
+	* Error to send
+	*/
+
+	$send_error = "Sorry, the email can not be sent.";
+
+	/**
+	* Debug
+	*/
+
+	$debug = true;
+?>
